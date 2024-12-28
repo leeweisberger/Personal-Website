84dedf1c01c992948ffee2cf0ab98fe36def79e8
Lee Weisberger <lee.weisberger@airtable.com>
2024-12-27 16:20:03 -0800

    Add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,15 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    integrations: [react(), tailwind()],
-    site: 'https://www.leeweisberger.com',
+  output: 'server',
+  integrations: [react(), tailwind()],
+  site: 'https://www.leeweisberger.com',
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
